772ffca7912a5ec4e81c824be5c38e62181f4ae9
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-03-18 15:35:05 -0500

    mock data and append person for each

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 
   'use strict';
 
-  const tmplPerson = document.querySelector('template.person');
-  const clonePerson = document.importNode(tmplPerson.content, true);
+  const createPerson = person => {
 
-  document.body.appendChild(clonePerson)
+    const templatePerson = document.querySelector('template.person');
+    const clonePerson = document.importNode(templatePerson.content, true);
+
+    return clonePerson;
+
+  };
+
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+
+      data.forEach(person => {
+
+        document.body.appendChild(createPerson(person));
+
+      })
+
+    })
 
 })();
