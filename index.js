fdb3b21cbbb9e6b0f6c6500bdda07c16575ce777
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-03-18 15:37:32 -0500

    insert value into template

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
     const templatePerson = document.querySelector('template.person');
     const clonePerson = document.importNode(templatePerson.content, true);
 
+    clonePerson.querySelector('.name').innerHTML = `${person.first_name} ${person.last_name}`;
+
     return clonePerson;
 
   };
